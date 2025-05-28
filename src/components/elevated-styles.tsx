5c406afee9fe396d96b1b62b952acb6e700a69a5
Derrick <pshubomi@gmail.com>
2025-05-28 07:53:49 +0100

    Working on the elevated style

---
+++ src/components/elevated-styles.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface ElevatedStyleProps {
+  hedaer: string;
+  image1: string;
+  image2: string;
+  content: string;
+  buttonText: string;
+  buttonLink: string;
+}
+
+function ElevatedStyle({
+  hedaer,
+  image1,
+  image2,
+  content,
+  buttonText,
+  buttonLink,
+}: ElevatedStyleProps) {}
+
+export default ElevatedStyle;
