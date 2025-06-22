7b2474302674e4119a6d7d974e380936452cc7a7
Derrick <pshubomi@gmail.com>
2025-06-22 14:35:41 +0100

    - worked on the pick-up and delivery screen

--- src/app/shop/delivery-option/page.tsx
+++ src/app/shop/delivery-option/page.tsx
@@ -1,27 +1,49 @@
 "use client";
 import Delivery from "@/components/shop/delivery-option/delivery";
+import Pickup from "@/components/shop/delivery-option/pick-up";
 import { useState } from "react";
 
 function DeliveryOption() {
   const [deliveryOption, setDeliveryOption] = useState("delivery");
 
   return (
-    <section className="min-h-screen flex items-center justify-center bg-primary">
-      <div className="max-w-4xl w-full mx-auto p-6 px-10 bg-white rounded-md shadow-md mt-10">
+    <section className="min-h-screen flex items-center justify-center py-20 bg-primary">
+      <div className="max-w-4xl w-full min-h-[120vh] mx-auto p-6 px-10 bg-white rounded-md shadow-md py-14">
         <h1 className="text-2xl font-bold mb-4 text-center">
           How would you like to get your products?
         </h1>
         <hr className="h-[2px] text-2xl bg-gray-300 max-w-xl mx-auto text-gray-300" />
 
         <div className="flex justify-center items-center my-8">
-          <button className="bg-secondary font-semibold py-2 px-6 cursor-pointer text-white">
+          <button
+            className={`
+              ${
+                deliveryOption === "delivery"
+                  ? "bg-secondary text-white"
+                  : "bg-gray-300"
+              } font-semibold py-2 px-6 cursor-pointer`}
+            onClick={() => setDeliveryOption("delivery")}
+          >
             Delivery
           </button>
-          <button className="bg-gray-300 font-semibold cursor-pointer py-2 px-6">
+          <button
+            className={`${
+              deliveryOption === "pickup"
+                ? "bg-secondary text-white"
+                : "bg-gray-300"
+            } font-semibold cursor-pointer py-2 px-6`}
+            onClick={() => setDeliveryOption("pickup")}
+          >
             Pick-up
           </button>
         </div>
-        <Delivery />
+        {deliveryOption === "delivery" ? <Delivery /> : <Pickup />}
+        <div className="flex gap-4 mt-20">
+          <button className="border border-secondary py-4 w-full">Back</button>
+          <button className="bg-secondary text-white py-4 w-full">
+            Continue to payment
+          </button>
+        </div>
       </div>
     </section>
   );
