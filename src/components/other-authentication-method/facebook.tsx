91c6d2ce61a1166a89650f1ef716e580211b5751
Derrick <pshubomi@gmail.com>
2025-12-18 21:32:21 +0100

    worked on facebook login and google login

--- src/components/other-authentication-method/facebook.tsx
+++ src/components/other-authentication-method/facebook.tsx
@@ -1,9 +1,16 @@
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FaFacebook } from "react-icons/fa";
 
 function Facebook() {
   return (
-    <button className="mt-4 w-full h-12 font-semibold rounded-2xl bg-secondary text-white border border-secondary flex justify-center items-center gap-2">
+    <button
+      type="button"
+      className="mt-4 w-full h-12 font-semibold rounded-2xl bg-secondary cursor-pointer text-white border border-secondary flex justify-center items-center gap-2"
+      onClick={() =>
+        signIn("facebook", { callbackUrl: "/other-verification-method" })
+      }
+    >
       <FaFacebook className="text-2xl text-white" /> Continue with Facebook
     </button>
   );
