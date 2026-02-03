141ba3c975c4049adee004d65a42ec61d1eb191b
Derrick <pshubomi@gmail.com>
2026-02-03 12:58:35 +0100

    worked on optimising the images

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "intertex-storage.s3.eu-north-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
         port: "",
         pathname: "/**",
       },
